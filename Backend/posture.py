449b95ce63acab76f83731f8321fdce6955afd8f
Amarjeet <amarjeetofficial81@gmail.com>
2025-07-10 12:26:40 +0530

    posture.py update

--- Backend/posture.py
+++ Backend/posture.py
@@ -1,6 +1,8 @@
 import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["MEDIAPIPE_DISABLE_GPU"] = "1"
+
 
 import sys
 import cv2
